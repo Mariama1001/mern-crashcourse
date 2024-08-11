b2f9872c211578e42954342d3ff9c73e984ac6ed
Mariama Diouf <mariamadiouf@MacBook-Air-de-Cheikh.local>
2024-08-11 22:56:56 +0200

    init

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,72 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./config/db.js";
+import Product from "./models/product.model.js";
+
+dotenv.config();
 
 const app = express();
 
-app.get("/", (req,res) => {
-    res.send("server is ready 123");
+app.use(express.json());
+
+
+app.get("/api/products" , async (req,res) => {
+    try {
+        const products = await Products.find({ });
+        res.status(200).json({success:true , data:products});
+    }
+        catch (error) {
+        console.log("Error in fetching products",error.message);
+        res.status(500).json({success:false , message:"Server error"});
+        
+    }
+
 });
 
+
+app.post("/api/products", async (req,res) => {
+    const product = req.body;
+
+if(!product.name || !product.price || !product.image){
+ return res.status(400).json({success:false , message:"Please provide all fields"});
+
+}
+const newProduct= new Product(product);
+
+try { 
+    
+    await newProduct.save();
+    return res.status(201).json({success:true , data:newProduct});
+} 
+catch (error) {
+
+    console.error("Error in Create product",error.message);
+    return res.status(500).json({success:false , message:"Server error"});
+    
+}
+    
+});
+
+app.delete("/api/products/:id" , async (req,res) => {
+    const {id} = req.params;
+
+    try { 
+    
+        await Product.findByIdAndDelete(id);
+        return res.status(200).json({success:true , message: "product deleted"});
+    } 
+    catch (error) {
+        console.log("error in deleting products:",error.message);
+        res.status(404).json({success:false , message: "product not found"});
+    
+}
+});
+
+
+
+console.log(process.env.MONGO_URI);
+
 app.listen(5000, () => {
-	
+    connectDB();
 	console.log("Server started at http://localhost:5000");
 });
