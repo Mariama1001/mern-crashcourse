6ca5a620cec07b3f146c92a543e288e163f46e93
Mariama Diouf <mariamadiouf@MacBook-Air-de-Cheikh.local>
2024-08-21 23:44:50 +0200

    init

--- Backend/server.js
+++ Backend/server.js
@@ -1,72 +1,30 @@
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
+
 import { connectDB } from "./config/db.js";
-import Product from "./models/product.model.js";
+
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
 const app = express();
+const PORT = process.env.PORT || 5000;
 
-app.use(express.json());
+const __dirname = path.resolve();
 
+app.use(express.json()); // allows us to accept JSON data in the req.body
 
-app.get("/api/products" , async (req,res) => {
-    try {
-        const products = await Products.find({ });
-        res.status(200).json({success:true , data:products});
-    }
-        catch (error) {
-        console.log("Error in fetching products",error.message);
-        res.status(500).json({success:false , message:"Server error"});
-        
-    }
+app.use("/api/products", productRoutes);
 
-});
-
-
-app.post("/api/products", async (req,res) => {
-    const product = req.body;
-
-if(!product.name || !product.price || !product.image){
- return res.status(400).json({success:false , message:"Please provide all fields"});
-
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
 }
-const newProduct= new Product(product);
 
-try { 
-    
-    await newProduct.save();
-    return res.status(201).json({success:true , data:newProduct});
-} 
-catch (error) {
-
-    console.error("Error in Create product",error.message);
-    return res.status(500).json({success:false , message:"Server error"});
-    
-}
-    
-});
-
-app.delete("/api/products/:id" , async (req,res) => {
-    const {id} = req.params;
-
-    try { 
-    
-        await Product.findByIdAndDelete(id);
-        return res.status(200).json({success:true , message: "product deleted"});
-    } 
-    catch (error) {
-        console.log("error in deleting products:",error.message);
-        res.status(404).json({success:false , message: "product not found"});
-    
-}
-});
-
-
-
-console.log(process.env.MONGO_URI);
-
-app.listen(5000, () => {
-    connectDB();
-	console.log("Server started at http://localhost:5000");
+app.listen(PORT, () => {
+	connectDB();
+	console.log("Server started at http://localhost:" + PORT);
 });
